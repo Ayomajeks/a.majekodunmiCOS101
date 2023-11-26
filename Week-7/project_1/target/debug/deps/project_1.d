fdd5c066a3181062ae4f641bd062b479902fa9c7
Ayomajeks <aymajeks@gmail.com>
2023-11-26 19:48:12 +0100

    (My Week_7 Projects)

---
+++ Week-7/project_1/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\AYO\Documents\a.majekodunmiCOS101\Week-7\project_1\target\debug\deps\project_1.exe: src\main.rs
+
+C:\Users\AYO\Documents\a.majekodunmiCOS101\Week-7\project_1\target\debug\deps\project_1.d: src\main.rs
+
+src\main.rs:
---
+++ Week-7/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\AYO\Documents\a.majekodunmiCOS101\Week-7\project_1\target\debug\project_1.exe: C:\Users\AYO\Documents\a.majekodunmiCOS101\Week-7\project_1\src\main.rs
